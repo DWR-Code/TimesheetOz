c7063b7768ab6f82c9e84e540820974097afd2d8
DWR-Code <ruffinolc@gmail.com>
2025-08-05 22:29:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,35 @@
-self.addEventListener('install', event => {
-  console.log('[ServiceWorker] Installed');
+const CACHE = 'ts-cache-v1';
+const ASSETS = [
+  '/', 
+  '/TimesheetOz/',
+  '/TimesheetOz/index.html',
+  '/TimesheetOz/manifest.json',
+  '/TimesheetOz/icon192.png',
+  '/TimesheetOz/icon512.png',
+  '/TimesheetOz/sw.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  console.log('[ServiceWorker] Activated');
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  // Serve solo la cache base
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r =>
+      r || fetch(e.request).then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      })
+    )
+  );
 });
